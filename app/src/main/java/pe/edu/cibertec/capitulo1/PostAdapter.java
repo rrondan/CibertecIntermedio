2f1716279d652f5f994498cfd367df23a28ed856
Richard Rondan Rojas <rondanrichard@gmail.com>
2019-09-29 13:29:21 -0500

    capitulo 1

---
+++ app/src/main/java/pe/edu/cibertec/capitulo1/PostAdapter.java
@@ -0,0 +1,51 @@
+package pe.edu.cibertec.capitulo1;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class PostAdapter extends RecyclerView.Adapter<PostAdapter.PostViewHolder> {
+
+    List<Post> postList;
+
+    public PostAdapter(List<Post> postList) {
+        this.postList = postList;
+    }
+
+    @NonNull
+    @Override
+    public PostViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_post, parent, false);
+        return new PostViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PostViewHolder holder, int position) {
+        Post post = postList.get(position);
+        holder.textViewTitle.setText(post.getTitle());
+        holder.textViewContent.setText(post.getText());
+    }
+
+    @Override
+    public int getItemCount() {
+        return postList.size();
+    }
+
+    public class PostViewHolder extends RecyclerView.ViewHolder {
+        private TextView textViewTitle;
+        private TextView textViewContent;
+
+        public PostViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textViewTitle = itemView.findViewById(R.id.textViewTitle);
+            textViewContent = itemView.findViewById(R.id.textViewContent);
+        }
+    }
+}
